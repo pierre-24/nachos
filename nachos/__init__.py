64261505680904d261642d98737854a6ca98aaf3
Pierre Beaujean <pierreb24@gmail.com>
2022-12-13 16:55:48 +0100

    switch to pyproject.toml

--- nachos/__init__.py
+++ nachos/__init__.py
@@ -11,34 +11,6 @@ __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
 __status__ = 'Development'
 
-# List of the scripts that are installed, without the .py extension. The name is used to give the command name.
-provide_scripts = [
-    'make',
-    'prepare',
-    'cook',
-    'bake',
-    'shake',
-    'analyze',
-    'peek'
-]
-
-
-def make_console_scripts(package_name='nachos'):
-    """Function used to generate the ``console_scripts`` list for ``setup.py``
-
-    :rtype: list
-    """
-
-    console_scripts = []
-    for script in provide_scripts:
-        path = os.path.join(package_name, script + '.py')
-        if not os.path.isfile(path):
-            raise FileNotFoundError(path)
-
-        console_scripts.append('{0} = {1}.{2}:main'.format('nachos_' + script, package_name, script))
-
-    return console_scripts
-
 
 def is_dir(dirname):
     """Checks if a path is an actual directory"""
